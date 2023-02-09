d056c28dacc32afba8fd9d568847a9bda8079fe7
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-02-09 13:59:53 +0530

    Add files via upload

--- springboot-smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ springboot-smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -1,32 +1,95 @@
 package com.smart.controller;
 
+import java.util.EmptyStackException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepo;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jakarta.websocket.Session;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private UserRepo userRepo;
+
 	@GetMapping("/")
-	public String home( Model m)
-	{
+	public String home(Model m) {
 		m.addAttribute("title", "Home-Smart Contact Manager");
 		return "home";
-		
+
 	}
-	
+
 	@GetMapping("/about")
-	public String about( Model m)
-	{
+	public String about(Model m) {
 		m.addAttribute("title", "About-Smart Contact Manager");
 		return "about";
-		
+
 	}
+
 	@GetMapping("/signup")
-	public String signup( Model m)
-	{
+	public String signup(Model m) {
 		m.addAttribute("title", "signup-Smart Contact Manager");
+		m.addAttribute("user", new User());
 		return "signup";
-		
+
+	}
+
+	// this handler for registering user
+
+	@PostMapping("/do_register")
+	public String registerUser( @Valid@ModelAttribute("user") User user,BindingResult result1,
+			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model m,
+			HttpSession session ) {
+
+		try {
+			if (!agreement) {
+				System.out.println("You Have Not Agreed Terms And Conditions");
+
+				throw new Exception("You Have Not Agreed Terms And Conditions");
+
+			}
+			
+			if(result1.hasErrors()) {
+				System.out.println("errors"+result1.toString());
+				m.addAttribute("user", user);
+				return"signup";
+			}
+
+			user.setRole("RoLe_User");
+			user.setEnabled(true);
+			user.setImageUrl("image");
+
+			System.out.println("Agreement" + agreement);
+			System.out.println("user" + user);
+
+			User save = this.userRepo.save(user);
+
+			m.addAttribute("user", new User());
+
+			session.setAttribute("message", new Message("Successfully Registered!!", "alert-success"));
+
+			return "signup";
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			m.addAttribute("user", user);
+			session.setAttribute("message", new Message("Somthing  Went Worng!!" + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+
 	}
 }
--- springboot-smartcontactmanager/src/main/java/com/smart/entities/User.java
+++ springboot-smartcontactmanager/src/main/java/com/smart/entities/User.java
@@ -12,6 +12,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="USER")
@@ -20,6 +22,8 @@ public class User {
 @GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+@NotBlank(message = "Name Field Is Required")
+@Size(min=2,max=20 ,message = "min 2 and max 20 characters are allowed!!! ")
 	private String name;
 	
 	@Column(unique = true)
