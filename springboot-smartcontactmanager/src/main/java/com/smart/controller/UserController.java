3c05018f6e9748665ec606265a8dc9fd17fdcd09
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-08-05 16:48:13 +0530

    Update UserController.java
    Signed-off-by: Ayush Gupta <121301901+ayush111195@users.noreply.github.com>

--- springboot-smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ springboot-smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 	@RequestMapping("/index")
-	public String dashboard() {
+	public  String dashboard() {
 		return "normal/user_dashboard";
 
 	}
