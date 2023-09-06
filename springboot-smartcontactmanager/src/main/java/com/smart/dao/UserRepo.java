32168e6d2986d4edc4d479cb4032dc3d44402856
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-09-07 01:21:28 +0530

    Update UserRepo.java

--- springboot-smartcontactmanager/src/main/java/com/smart/dao/UserRepo.java
+++ springboot-smartcontactmanager/src/main/java/com/smart/dao/UserRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.User;
 
-public interface UserRepo  extends JpaRepository<User, Integer>{
+public interface UserRepo extends JpaRepository<User, Integer>{
 	
 	
 	// Make A Query Get A All User By Name -->>
